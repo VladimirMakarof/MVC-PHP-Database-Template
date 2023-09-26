a1f6138616e45050abcecd48e6c42f7c13ce046d
Vladimir_Makarov <stan13.09@mail.ru>
2023-09-26 15:00:09 +0300

    Fixed CSS Styling

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -8,11 +8,11 @@ class UserController
   public function __construct($conn)
   {
     $this->model = new UserModel($conn);
+    session_start(); // Старт сессии
   }
 
   public function handleRequest()
   {
-    $successMessage = "";
     $errorMessage = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -25,12 +25,13 @@ class UserController
       } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $errorMessage = "Неправильный формат email-адреса.";
       } elseif ($this->model->insertUser($name, $email, $phone)) { // Обновление метода insertUser
-        $successMessage = "Данные успешно записаны в базу данных";
+        $_SESSION['successMessage'] = "Данные успешно записаны в базу данных"; // Сохраняем сообщение в сессии
+        header('Location: index.php'); // Редирект на index.php
+        exit;
       } else {
         $errorMessage = "Ошибка при записи данных в базу данных.";
       }
     }
-
 
     // Включение HTML-шаблона
     include 'views/index.php';
--- views/index.php
+++ views/index.php
@@ -6,12 +6,13 @@
   <title>Форма для записи в базу данных</title>
   <link href="/public/css/style.css" rel="stylesheet">
   <link rel="stylesheet" href="/public/css/burger-menu.css">
-
-
+  <link rel="stylesheet" href="/public/css/form.css">
 </head>
 
 <body>
-  <?php include 'header.php'; ?>
+  <?php
+  include 'header.php';
+  ?>
   <h1>Запись данных в базу данных</h1>
   <form action="index.php" method="post">
     <label for="name">Имя:</label>
@@ -20,24 +21,25 @@
     <label for="phone">Номер телефона:</label>
     <input type="tel" name="phone" required><br>
 
-
     <label for="email">Email:</label>
     <input type="email" name="email" required><br>
 
     <!-- Отображение сообщения об успешной записи -->
-    <?php if ($successMessage) : ?>
-      <p><?= $successMessage ?></p>
+    <?php if (isset($_SESSION['successMessage'])) : ?>
+      <p class="success"><?= $_SESSION['successMessage'] ?></p>
+      <?php unset($_SESSION['successMessage']); // Очищаем сообщение из сессии 
+      ?>
     <?php endif; ?>
 
     <!-- Отображение сообщения об ошибке -->
     <?php if ($errorMessage) : ?>
-      <p><?= $errorMessage ?></p>
+      <p class="error"><?= $errorMessage ?></p>
     <?php endif; ?>
+
 
     <input type="submit" value="Отправить"><br>
   </form>
   <script src="/public/js/burger-menu.js"></script>
-
 </body>
 
 </html>
